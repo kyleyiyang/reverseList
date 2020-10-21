049d02725d10c6d289134c510f973aa5401f3363
kyleyiyang <kyleyiyang@gmail.com>
2020-10-21 15:35:46 -0400

    Update reverseList.java

--- reverseList.java
+++ reverseList.java
@@ -22,3 +22,34 @@ class Solution {
         return h;
     }
 }
+
+// recursion
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public static ListNode h = new ListNode();
+    public ListNode reverseList(ListNode head) {
+        if (head==null) {
+            return null;
+        }
+        
+        h.val=head.val;
+        recur(head);
+        return h;
+    }
+    public static void recur(ListNode hea) {
+        if (hea.next!=null) {
+            h=new ListNode(hea.next.val,h);
+            recur(hea.next);
+        }
+    }
+}
